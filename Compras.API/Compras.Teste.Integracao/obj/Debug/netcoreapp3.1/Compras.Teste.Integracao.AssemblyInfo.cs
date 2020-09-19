0d3c03b14af0d888363e1de2625ba628b4db28de
Alana rodrigues <alana.rodrigues@lambda3.com.br>
2020-09-19 16:31:51 -0300

    remover bin e obj

--- Compras.API/Compras.Teste.Integracao/obj/Debug/netcoreapp3.1/Compras.Teste.Integracao.AssemblyInfo.cs
+++ Compras.API/Compras.Teste.Integracao/obj/Debug/netcoreapp3.1/Compras.Teste.Integracao.AssemblyInfo.cs
@@ -11,7 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("Compras.API, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "C:\\Projetos\\compras\\Compras.API\\Compras.API", "Compras.API.csproj", "0")]
+[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("Compras.API", "C:\\Projetos\\compras\\Compras.API\\Compras.API", "Compras.API.csproj", "0")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Compras.Teste.Integracao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
